740234ac03b74048f4078442c72e563a280a3309
Motus Pilobolus <kayla.prata@gmail.com>
2022-03-25 22:01:54 -0400

    implement all mandatory exercise functionality

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
-function addGridBlock() {
+function addGridBlock(hw) {
     const gridBlock=document.createElement('div');
     const containerBlock=document.getElementById('16by16');
     gridBlock.classList.add('grid-item');
+    gridBlock.style.height=hw;
+    gridBlock.style.width=hw;
     gridBlock.addEventListener('mouseenter', function( event ) {
         this.classList.add('hovered');
     })
     containerBlock.appendChild(gridBlock);
+    console.log('added grid block!')
 }
 
-function iterativeGridBlock(x) {
-for (let i=0; i < x; i++) { addGridBlock();}
+function iterativeGridBlock(x, hw) {
+for (let i=0; i < x; i++) { addGridBlock(hw);}
 }
 
 function clearGridBlock() {
@@ -29,17 +32,15 @@ btn.onclick = function() {
 
 pxlBtn.onclick = function() {
     var pixelRequest = document.getElementById("pixelRequest").value;
-    iterativeGridBlock(pixelRequest * pixelRequest);
-    
+    if (pixelRequest > 100) {pixelRequest = 100
+        alert('Can\'t go higher than 100!');
+    }
+    var heightWidth = `${560 / pixelRequest}px`;
+    var gridNumber = pixelRequest * pixelRequest
+    iterativeGridBlock(gridNumber, heightWidth);
     modal.style.display = "none";
 }
     
-window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-
 document.addEventListener('DOMContentLoaded', function () {
-iterativeGridBlock(256);
+iterativeGridBlock(256, "36px");
 });
